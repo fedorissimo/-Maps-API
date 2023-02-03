56d341e1bc46daebf453e8de75673d3320a251a3
fedorissimo <91797262+fedorissimo@users.noreply.github.com>
2023-02-03 19:01:25 +0500

    Номер 2 Ограничения

--- Часть 2.py
+++ Часть 2.py
@@ -26,29 +26,31 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
-                delta = float(delta)
-                delta /= 1.5
-                delta = str(delta)
-                params = {
-                    'll': ",".join([lon, lat]),
-                    'spn': ",".join([delta, delta]),
-                    'l': 'map'}
-                response = requests.get(api_server, params=params)
-                file = open('map.png', 'wb')
-                file.write(response.content)
-                file.close()
+                if float(delta) > 0.0001:
+                    delta = float(delta)
+                    delta /= 1.5
+                    delta = str(delta)
+                    params = {
+                        'll': ",".join([lon, lat]),
+                        'spn': ",".join([delta, delta]),
+                        'l': 'map'}
+                    response = requests.get(api_server, params=params)
+                    file = open('map.png', 'wb')
+                    file.write(response.content)
+                    file.close()
             if event.key == pygame.K_PAGEDOWN:
-                delta = float(delta)
-                delta *= 2
-                delta = str(delta)
-                params = {
-                    'll': ",".join([lon, lat]),
-                    'spn': ",".join([delta, delta]),
-                    'l': 'map'}
-                response = requests.get(api_server, params=params)
-                file = open('map.png', 'wb')
-                file.write(response.content)
-                file.close()
+                if float(delta) < 43:
+                    delta = float(delta)
+                    delta *= 2
+                    delta = str(delta)
+                    params = {
+                        'll': ",".join([lon, lat]),
+                        'spn': ",".join([delta, delta]),
+                        'l': 'map'}
+                    response = requests.get(api_server, params=params)
+                    file = open('map.png', 'wb')
+                    file.write(response.content)
+                    file.close()
 
     screen.blit(pygame.image.load('map.png'), (0, 0))
     pygame.display.flip()
